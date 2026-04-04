9354a66fba911d8f39a0dca3d016834062969468
Sadith Nuñez <sadnr0213@gmail.com>
2026-04-04 16:23:36 -0500

    refactor: update OrderStatus import path to @prisma/client and remove unused validation decorators

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,6 +1,6 @@
 import { Type } from "class-transformer";
-import { IsBoolean, IsDate, IsEnum, IsNumber, IsOptional, IsPositive, IsString } from "class-validator";
-import { OrderStatus } from "generated/prisma";
+import { IsBoolean, IsEnum, IsNumber, IsOptional, IsPositive } from "class-validator";
+import { OrderStatus } from "@prisma/client";
 import { OrderStatusList } from "../enums/order.enum";
 
 export class CreateOrderDto {
